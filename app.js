ee5eefbcf2aa23d5686a540edf6f14011f436f94
weejh <wee_jh@yahoo.com>
2015-12-04 09:19:14 +0800

    Revert "app.js"
    This reverts commit 5152fc6cd4d32de903f2fd4bccd69267f19cce87.

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ var totalTwo = 0
 document.getElementById('score1').textContent = 1
 document.getElementById('score1').color = 'black'
 
-// Add vote function - Button 1
-function addVoteOne () {
-  totalOne = totalOne + 1
-}
-choiceOne.onclick = addVoteOne()
-
-// Add vote function - Button 2
-function addVoteTwo () {
-  totalTwo = totalTwo + 1
-}
-choiceTwo.onclick = addVoteTwo()
-
-// Reset button with function
-function reset () {
-  totalOne = 0
-  totalTwo = 0
-}
-reset.onclick = reset()
+// // Add vote function - Button 1
+// function addVoteOne () {
+//   totalOne = totalOne + 1
+// }
+// // choiceOne.onclick = addVoteOne()
+//
+// // Add vote function - Button 2
+// function addVoteTwo () {
+//   totalTwo = totalTwo + 1
+// }
+// // choiceTwo.onclick = addVoteTwo()
+//
+// // Reset button with function
+// function reset () {
+//   totalOne = 0
+//   totalTwo = 0
+// }
+// // reset.onclick = reset()
